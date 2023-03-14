3c2589ca696d5ae5d656fc6147b8e92c0b264bfb
Mateo Díaz <mateodiaz401@gmail.com>
2023-03-14 10:58:25 -0500

    rename mi portafolio

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/portafolio-web'),
+      dir: require('path').join(__dirname, './coverage/web-portafolio'),
       subdir: '.',
       reporters: [
         { type: 'html' },
